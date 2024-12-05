e6231a612e8a48a165bd7057e876799e9738f17a
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2024-12-04 21:32:29 -0300

    type: modify module module: LogisticRegrecion description: - add Eq derivation on Net Type - reverse rundom values em newRandomNet - reverse order of treatTarget function - modify signatura of predict function - rename train function to trainStep - criate a new train function

--- library/LogisticRegrecion.hs
+++ library/LogisticRegrecion.hs
@@ -4,13 +4,13 @@ import System.Random (randoms, mkStdGen)
 
 data (RealFrac v) => TrainConfig v =  TrainConfig{trainAlpha::v, trainN::v}
 
-data (RealFrac a, Show a, Read a) => Net a =  Net [[a]] [a] deriving (Show, Read)
+data (RealFrac a, Show a, Read a) => Net a =  Net [[a]] [a] deriving (Show, Read, Eq)
 
 newRandomNet:: Int -> Int -> Net Float
 newRandomNet inputN outputN = 
     let
         rlist:: Int -> [Float]
-        rlist l = fmap fromInteger $ take l $ randoms $ mkStdGen 10 -- shold fix thet
+        rlist l = fmap ((1/) . fromInteger) $ take l $ randoms $ mkStdGen 10 -- shold fix thet
         bs  = rlist outputN
         wss = [rlist inputN | _ <- [1 .. outputN]] 
     in
@@ -19,18 +19,18 @@ newRandomNet inputN outputN =
 treatTarget:: RealFrac a  => a -> [a]
 treatTarget a = 
     let
-        aux _ (-1) = []
+        aux _ (10) = []
         aux n b
-            | n == b    = 1:aux n (b-1)
-            | otherwise = 0:aux n (b-1)
+            | n == b    = 1:aux n (b+1)
+            | otherwise = 0:aux n (b+1)
     in
-        aux a 9
+        aux a 0
 
 softmax:: RealFrac a => [a] -> [a]
 softmax as =
     let
         maxV = foldl max 0 as
-        softEach a = exp $ realToFrac $ realToFrac  a - maxV
+        softEach v = exp $ realToFrac $ realToFrac  v - maxV
         softenValues = realToFrac . softEach <$> as
         sumV = sum softenValues
     in
@@ -45,14 +45,14 @@ sigmoid (ws:wss) (b:bs) xs =
         dot + b : sigmoid wss bs xs
 sigmoid _ _ _ = error "for all weights[n][m] shoud be bias[n]!" -- pending: handle exceptions
 
-predic:: RealFrac a => [[a]] -> [a] -> [a] -> [a]
-predic ws bs xs = softmax $ sigmoid ws bs xs
+predic:: (RealFrac a, Show a, Read a) => Net a -> [a] -> [a]
+predic (Net wss bs) xs = softmax $ sigmoid wss bs xs
 
-train:: (RealFrac a, Show a, Read a) => TrainConfig a -> Net a -> [a] -> a -> Net a
-train tc (Net wss bs) xs y =
+trainStep:: (RealFrac a, Show a, Read a) => TrainConfig a -> Net a -> [a] -> a -> Net a
+trainStep tc net@(Net wss bs) xs y =
     let
         targetY = treatTarget y
-        ys = predic wss bs xs
+        ys = predic net xs
         deltasYs = zipWith (-) targetY ys
         lc = trainAlpha tc / trainN tc
         fixW dy ws  =
@@ -63,3 +63,11 @@ train tc (Net wss bs) xs y =
         fixedBs = zipWith (+) bs $ (lc*) <$> deltasYs 
     in
         Net ( zipWith ($) (fixW <$> deltasYs) wss) fixedBs
+
+train:: (RealFrac a, Show a, Read a) => TrainConfig a -> Net a -> Net a -> Net a
+train tconf net (Net (xs:xss) (y:ys)) = 
+    let 
+        netStep = trainStep tconf net xs y
+    in
+        train tconf netStep (Net xss ys)
+train _ n _ = n
