1c7be977ec295ecf07ea997c00c7f51371323045
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2024-12-04 21:39:27 -0300

    type: modify module module: Main description: test all modifications on main

--- executable/Main.hs
+++ executable/Main.hs
@@ -6,13 +6,31 @@ import  LogisticRegrecion.IO
 import LogisticRegrecion
 
 trainDataFile = "./data/train.csv"
+initilaNetFile = "./data/initilaNet.csv"
+netFile = "./data/net.csv"
+testDataFile = "./data/test.csv"
 
 inputN  = 784
 outputN = 10
 
+--initialNet = newRandomNet 784 10
+
+config = TrainConfig {
+    trainAlpha = 0.1,
+    trainN = 800
+}
+
+printResult (ys:yss) (t:ts) = do
+    putStrLn $ show ys ++ ' ':show t
+    printResult yss ts
+printResult _ _ = return ()
+
 main :: IO ()
 main = do
-    net <- readNet trainDataFile 2
-    print net 
-
-
+    trainData <- readNet trainDataFile 800
+    initialNet <- readNet initilaNetFile   10
+    (Net testData  targest) <- readNet testDataFile 80
+    let trained = train config initialNet trainData
+    let test = predic trained <$> testData
+    saveNet netFile  trained
+    printResult test targest
